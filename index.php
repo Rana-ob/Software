ed48a1a5cdd63749bdc87d17c481ead18211fcdf
Janosiw <152038154+Janosiw@users.noreply.github.com>
2025-03-29 04:42:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
-$dbname = "databaseS";
+$dbname = "database";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
